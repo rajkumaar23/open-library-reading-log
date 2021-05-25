26045494aaff482879cf5ddf50977a23787bfd34
Rajkumar <rajkumaar2304@icloud.com>
2021-05-25 20:15:18 +0530

    add pull step to update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /usr/bin/php index.php
+git pull origin main
 git add .
 git commit -m "$(date +"%D %T") updating reading log" || echo "No changes, nothing to commit!"
 git push origin main
