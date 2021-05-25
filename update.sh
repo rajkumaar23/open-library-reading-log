bbf3208e072f5f3c8536d0ad1737d14348f1eb76
Rajkumar S <rajkumaar2304@icloud.com>
2021-05-25 22:12:10 +0530

    Add cron.yml github workflow

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/usr/bin/php index.php
-git pull origin main
-git add .
-git commit -m "$(date +"%D %T") updating reading log" || echo "No changes, nothing to commit!"
-git push origin main
