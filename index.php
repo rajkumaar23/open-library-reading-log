850e22b497f3218bf22c567cb693cc1c166c11d1
Rajkumar S <rajkumaar2304@icloud.com>
2021-07-22 09:32:20 +0530

    Handle empty values

--- index.php
+++ index.php
@@ -13,12 +13,18 @@ try {
             $link = "$domain/books/{$entry->work->cover_edition_key}";
             $authors = $entry->work->author_names;
             $image = "https://covers.openlibrary.org/b/id/" . json_decode(file_get_contents("$link.json"))->covers[0] . "-L.jpg";
-            $data[$type][] = compact('image', 'title', 'link', 'authors');
+            if (!empty($image) && !empty($title) && !empty($link) && !empty($authors)) {
+                $data[$type][] = compact('image', 'title', 'link', 'authors');
+            } else {
+                exit(1);
+            }
         }
     }
     header('Content-Type: application/json');
     echo json_encode($data);
-    file_put_contents(__DIR__ . "/_data/reading-log.json", json_encode($data, JSON_PRETTY_PRINT));    
+    if (!empty($data)) {
+        file_put_contents(__DIR__ . "/_data/reading-log.json", json_encode($data, JSON_PRETTY_PRINT));
+    }
 } catch (Exception $exception) {
     echo $exception->getMessage();
 }
